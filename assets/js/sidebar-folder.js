37fe648e225b5a1244c84293a911193c597082fd
nomoreFt <hyunwoo.kim1108@gmail.com>
2023-09-29 19:14:11 +0900

    feat : add post + add swipe.png

--- assets/js/sidebar-folder.js
+++ assets/js/sidebar-folder.js
@@ -1,7 +1,22 @@
+/*
+dropdown이 default로 닫혀있을 경우
 function spread(count){
     document.getElementById('folder-checkbox-' + count).checked =
         !document.getElementById('folder-checkbox-' + count).checked
     document.getElementById('spread-icon-' + count).innerHTML =
         document.getElementById('spread-icon-' + count).innerHTML == 'arrow_right' ?
             'arrow_drop_down' : 'arrow_right'
+}
+*/
+
+
+/*
+dropdown이 default로 열려있을 경우
+ */
+function spread(count){
+    document.getElementById('folder-checkbox-' + count).checked =
+        !document.getElementById('folder-checkbox-' + count).checked;
+    document.getElementById('spread-icon-' + count).innerHTML =
+        document.getElementById('spread-icon-' + count).innerHTML == 'arrow_drop_down' ?
+            'arrow_right' : 'arrow_drop_down';
 }
